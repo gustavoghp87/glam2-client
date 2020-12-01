329d4d8ae13a24b8b4dbd629e1495162a7e0ae4b
gustavoghp87 <ghp.2120@gmail.com>
2020-12-01 13:10:08 -0300

    regreso a server1

--- src/hoc/Config.js
+++ src/hoc/Config.js
@@ -1,7 +1,7 @@
 
 //SERVER ROUTES
 
-const base = 'https://glam2-rest-api2.herokuapp.com'
+const base = 'https://glam2-rest-api.herokuapp.com'
 //const base = 'http://localhost:5000'
 
 export const USER_SERVER = `${base}/api/users`
